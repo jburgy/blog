65410aacc5af831f66b6399bf6405e885a46484b
Jan Burgy <jburgy@gmail.com>
2019-12-30 13:34:33 +0000

    Bare bones package for practice.

---
+++ aioutils/asyncify.py
@@ -0,0 +1,30 @@
+from asyncio import get_event_loop
+from concurrent.futures import ThreadPoolExecutor
+from contextlib import closing
+from functools import partial, wraps
+from inspect import signature
+
+
+def asyncify(f):
+    loop = get_event_loop()
+    @wraps(f)
+    def wrapper(*args, **kwds):
+        return loop.run_in_executor(None, partial(f, *args, **kwds))
+    wrapper.__signature__ = signature(f)
+    return wrapper
+
+
+@asyncify
+def readall(filename):
+    with open(filename) as lines:
+        return '\n'.join(line for line in lines)
+
+
+if __name__ == '__main__':
+    help(readall)
+    print(signature(readall))
+    with closing(get_event_loop()) as loop:
+        with ThreadPoolExecutor() as executor:
+            loop.set_default_executor(executor)
+            content = loop.run_until_complete(readall('/home/vsonline/workspace/environment.yml'))
+            print(content)
