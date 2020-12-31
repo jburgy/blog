94339f52d3e232e3f4b30dc821cc1f56c729d7c8
Jan Burgy <jburgy@gmail.com>
2020-12-31 16:17:49 -0500

    Fix pytest

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -52,6 +52,30 @@ def propagate(possible: np.array, count: ma.array, where: ma.array) -> int:
 
 
 def solve(given: np.array) -> np.array:
+    """ Solve a sudo puzzle using backtracking
+
+    >>> i = np.loadtxt(io.StringIO('''
+    ... 5 3 0 0 7 0 0 0 0
+    ... 6 0 0 1 9 5 0 0 0
+    ... 0 9 8 0 0 0 0 6 0
+    ... 8 0 0 0 6 0 0 0 3
+    ... 4 0 0 8 0 3 0 0 1
+    ... 7 0 0 0 2 0 0 0 6
+    ... 0 6 0 0 0 0 2 8 0
+    ... 0 0 0 4 1 9 0 0 5
+    ... 0 0 0 0 8 0 0 7 9
+    ... '''), dtype=np.uint8)
+    >>> solve(i)
+    array([[5, 3, 4, 6, 7, 8, 9, 1, 2],
+           [6, 7, 2, 1, 9, 5, 3, 4, 8],
+           [1, 9, 8, 3, 4, 2, 5, 6, 7],
+           [8, 5, 9, 7, 6, 1, 4, 2, 3],
+           [4, 2, 6, 8, 5, 3, 7, 9, 1],
+           [7, 1, 3, 9, 2, 4, 8, 5, 6],
+           [9, 6, 1, 5, 3, 7, 2, 8, 4],
+           [2, 8, 7, 4, 1, 9, 6, 3, 5],
+           [3, 4, 5, 2, 8, 6, 1, 7, 9]])
+    """
     possible = np.full((9, 9, 9), True)
     mask = given > 0
     possible[mask, :] = False
