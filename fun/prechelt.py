b58a047f3a3212ecd9636ffd6d5ab983c3af5bec
Jan Burgy <jburgy@gmail.com>
2021-10-09 20:04:02 -0400

    Recursion is so elegant

--- fun/prechelt.py
+++ fun/prechelt.py
@@ -7,9 +7,7 @@ MAP = str.maketrans(
 
 
 def overlapping(haystack: str, needle: str, n: int) -> Iterator[str]:
-    pattern = needle.translate(MAP)
-
-    i = -1
+    pattern, i = needle.translate(MAP), -1
     while True:
         i = haystack.find(pattern, i + 1)
         if i < 0:
@@ -17,23 +15,16 @@ def overlapping(haystack: str, needle: str, n: int) -> Iterator[str]:
         yield haystack[:i] + needle + haystack[i + n:]
 
 
-def replaceall(haystack: str, *needles: str) -> Iterator[str]:
+def replaceall(haystack: str, needle: str, *needles: str) -> Iterator[str]:
     """
     >>> list(replaceall("358675", "Dali", "um", "Sao", "da", "Pik"))
     ['daPik5', 'Sao6um', 'Dalium']
     """
-    m, n = sum(ch.isdigit() for ch in haystack), len(needles)
-    stack = [(haystack, m, i) for i in range(n)]  # try all needles on entire haystack
-    while stack:
-        haystack, m, i = stack.pop()
-        needle = needles[i]
-        i += 1  # try next needle on overlapping replacements
-        k = len(needle)
-        m -= k  # remove k digits ⇒ reduce search space
-        if m < 0 or (m > 1 and i == n):
-            continue
-        replacements = overlapping(haystack, needle, k)
-        if m <= 1:  # 0 or 1 digits left, habemus solutiones!
-            yield from replacements
-        else:
-            stack.extend((haystack, m, i) for haystack in replacements)
+    n = len(needle)
+    replacements = overlapping(haystack, needle, n)
+    if sum(map(str.isdigit, haystack), -n) <= 1:
+        yield from replacements
+    elif needles:
+        yield from replaceall(haystack, *needles)
+        for replacement in replacements:
+            yield from replaceall(replacement, *needles)
