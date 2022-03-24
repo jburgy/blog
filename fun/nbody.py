75c5ab2e5e0953c4ddbed760466c377083462768
Jan Burgy <jburgy@gmail.com>
2022-03-23 22:42:47 -0400

    Avoid all copies in innermost loop.

--- fun/nbody.py
+++ fun/nbody.py
@@ -5,7 +5,7 @@
 # modified by Tupteq, Fredrik Johansson, and Daniel Nanz
 # modified by Maciej Fijalkowski
 
-from numpy import array, divide, empty, multiply, pi, r_, sqrt, sum
+from numpy import array, divide, empty, greater, multiply, pi, r_, sqrt, sum, take
 from scipy.linalg.blas import daxpy, dspr2, zhpmv, zhpr
 
 x = array([
@@ -14,58 +14,59 @@ x = array([
     [8.34336671824457987e+00, 4.12479856412430479e+00, -4.03523417114321381e-01],
     [1.28943695621391310e+01, -1.51111514016986312e+01, -2.23307578892655734e-01],
     [1.53796971148509165e+01, -2.59193146099879641e+01, 1.79258772950371181e-01],
-])
+]).T
 v = array([
     [0.0, 0.0, 0.0],
     [1.66007664274403694e-03, 7.69901118419740425e-03, -6.90460016972063023e-05],
     [-2.76742510726862411e-03, 4.99852801234917238e-03, 2.30417297573763929e-05],
     [2.96460137564761618e-03, 2.37847173959480950e-03, -2.96589568540237556e-05],
     [2.68067772490389322e-03, 1.62824170038242295e-03, -9.51592254519715870e-05],
-]) * .36524
+]).T * .36524
 m = array([
-    [1.0],
-    [9.54791938424326609e-04],
-    [2.85885980666130812e-04],
-    [4.36624404335156298e-05],
-    [5.15138902046611451e-05],
+    1.0,
+    9.54791938424326609e-04,
+    2.85885980666130812e-04,
+    4.36624404335156298e-05,
+    5.15138902046611451e-05,
 ]) * 4 * pi**2
 
-v[:, 0] = -sum(m * v, axis=1) / m[:, 0]
+v[:, 0] = -sum(m * v, axis=1) / m[0]
 a = empty(x.shape, dtype=complex)
 
 jm = m * -1j
-z = empty(x.shape, dtype=complex)
 n = x.shape[1]
 # see https://stackoverflow.com/a/52564537/8479938
 # only need upper triangle plus diagonal hence ½ n x (n + 1)
 ap = empty((3, n * (n + 1) // 2), dtype=complex)
 xxT = empty(ap.shape[1], dtype=float)
+where = empty(ap.shape, dtype=bool)
 d = empty(ap.shape[1], dtype=float)
 trid = r_[0, 2: n + 1].cumsum()  # triangular diagonal
-ones = empty((n,), dtype=float)
+x2 = empty(n, dtype=float)
+ones = empty(n, dtype=float)
 ones.fill(1.0)
 
 dt = 0.01
 
 for _ in range(20_000):
-    # (x - i)(y + i) = x y + 1 + i(x - y)
-    # (x - y)² = x² + y² - 2xy
-
-    z.real = x
-    z.imag.fill(-1.0)
+    a.real = x
+    a.imag.fill(-1.0)
     ap.fill(0.0)
     # A += α z z*
-    zhpr(n, alpha=-2.0, x=z[0], ap=ap[0], overwrite_ap=True)
-    zhpr(n, alpha=-2.0, x=z[1], ap=ap[1], overwrite_ap=True)
-    zhpr(n, alpha=-2.0, x=z[2], ap=ap[2], overwrite_ap=True)
+    zhpr(n, alpha=-2.0, x=a[0], ap=ap[0], overwrite_ap=True)
+    zhpr(n, alpha=-2.0, x=a[1], ap=ap[1], overwrite_ap=True)
+    zhpr(n, alpha=-2.0, x=a[2], ap=ap[2], overwrite_ap=True)
     sum(ap.real, axis=0, out=xxT)
     xxT += 6
     # A += α (x yᵀ + y xᵀ)
-    dspr2(n, alpha=-0.5, x=xxT[trid], y=ones, ap=xxT, overwrite_ap=True)
+    take(xxT, trid, out=x2)
+    dspr2(n, alpha=-0.5, x=x2, y=ones, ap=xxT, overwrite_ap=True)
 
     sqrt(xxT, out=d)
     multiply(xxT, d, out=xxT)
-    divide(ap.imag, xxT, where=xxT.astype(bool), out=ap.imag)
+    greater(xxT, 0.0, out=where)
+
+    divide(ap.imag, xxT, where=where, out=ap.imag)
 
     # y = α A x + β y
     zhpmv(n, alpha=0.5, ap=ap[0], x=jm, beta=0.0, y=a[0], overwrite_y=True)
