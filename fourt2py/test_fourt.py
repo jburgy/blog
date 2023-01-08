80afdec9462ac2dfbdbdaf14c857d7fc3bc9ceab
Jan Burgy <jburgy@gmail.com>
2023-01-08 18:04:49 -0500

    Another attempt to fix github actions.

--- fourt2py/test_fourt.py
+++ fourt2py/test_fourt.py
@@ -2,9 +2,10 @@ import numpy as np
 
 from fourt2py import fourt
 
-a = np.zeros(15, dtype=complex)
-a[1] = 1
-a[3] = -1
 
-print(fourt(a))
-print(np.fft.fft(a))
+def test_fourt():
+    a = np.zeros(15, dtype=complex)
+    a[1] = 1
+    a[3] = -1
+
+    assert np.allclose(fourt(a), np.conjugate(np.fft.fft(a)))
