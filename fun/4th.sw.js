32b0b4d95ed2acd7a4da01f3e63059410abd8e90
Jan Burgy <jburgy@gmail.com>
2023-02-21 06:59:08 -0500

    Specialized server is not necessary

---
+++ fun/4th.sw.js
@@ -0,0 +1,24 @@
+self.addEventListener("install", () => self.skipWaiting());
+
+self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
+        return;
+    }
+
+    event.respondWith(
+        fetch(event.request).then(
+            (response) => {
+                const newHeaders = new Headers(response.headers);
+                newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+                newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+
+                return new Response(response.body, {
+                    status: response.status,
+                    statusText: response.statusText,
+                    headers: newHeaders,
+                });
+            }, console.error)
+    );
+});
