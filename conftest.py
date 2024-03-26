003d2869d01b28de2a4734c524219ff9947e2b15
Jan Burgy <jburgy@gmail.com>
2024-03-26 06:20:27 -0400

    Pass tests except DOES>

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ import pytest
 def pytest_addoption(parser: pytest.Parser):
     parser.addoption(
         "--target",
-        choices=["4th", "4th.gcov", "4th.32", "4th.ll"],
+        choices=["4th", "4th.gcov", "4th.32", "4th.ll", "5th.ll"],
         default="4th",
         help="Pick a target to test",
     )
