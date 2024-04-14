0944b88e9a270e80390ce5cc0835b9e29acaaad5
Jan Burgy <jburgy@gmail.com>
2024-04-14 12:38:46 -0400

    Upgrade emsdk and xterm-pty to fix 5th.wasm

--- fun/5th.worker.js
+++
@@ -1,9 +0,0 @@
-importScripts("https://cdn.jsdelivr.net/npm/xterm-pty@0.9.4/workerTools.js");
-
-// https://developer.mozilla.org/en-US/docs/Web/API/WorkerGlobalScope/self
-// self.addEventListener("message", ...) looks less magic than onmessage = ... to me
-self.addEventListener("message", (msg) => {
-    importScripts(location.origin + "/5th.js");
-
-    emscriptenHack(new TtyClient(msg.data));
-});
