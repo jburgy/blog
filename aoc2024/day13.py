ba1e43792fda2e4acf8dcd9a78b67841c457a571
Jan Burgy <jburgy@gmail.com>
2024-12-13 10:49:53 -0500

    Simplify Day 13 of Advent of Code

--- aoc2024/day13.py
+++ aoc2024/day13.py
@@ -11,23 +11,23 @@ with open("aoc2024/day13input.txt", "rt") as lines:
         if matches is None:
             pass
         elif matches["type"] == "Button A":
-            a00 = Fraction(matches["x"])
-            a10 = Fraction(matches["y"])
+            a00 = int(matches["x"])
+            a10 = int(matches["y"])
+            pivot = Fraction(a10, a00)
         elif matches["type"] == "Button B":
-            a01 = Fraction(matches["x"])
-            a11 = Fraction(matches["y"])
+            a01 = int(matches["x"])
+            a11 = int(matches["y"])
         elif matches["type"] == "Prize":
-            b0 = Fraction(matches["x"]) + add
-            b1 = Fraction(matches["y"]) + add
+            b0 = int(matches["x"]) + add
+            b1 = int(matches["y"]) + add
 
-            pivot = a10 / a00
             c1 = (b1 - pivot * b0) / (a11 - pivot * a01)
             c0 = (b0 - c1 * a01) / a00
 
             cost += (
                 c0.numerator * 3 + c1.numerator
-                if c0.denominator == 1
-                and c1.denominator == 1
+                if c0.is_integer()
+                and c1.is_integer()
                 else 0
             )
 
