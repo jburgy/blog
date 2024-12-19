07e5d16836935cac26497359fb1263b2a33c9d11
Jan Burgy <jburgy@gmail.com>
2024-12-19 12:28:04 -0500

    Day 19 of Advent of Code

---
+++ aoc2024/day19.py
@@ -0,0 +1,29 @@
+from collections.abc import Callable
+from functools import cache
+
+
+def counter(needles: list[str]) -> Callable[[str], int]:
+
+    @cache
+    def count(haystack: str) -> int:
+        return (
+            sum(
+                count(haystack[len(needle):])
+                for needle in needles
+                if haystack.startswith(needle)
+            )
+            if haystack
+            else 1
+        )
+
+    return count
+
+
+with open("aoc2024/day19input.txt", "rt") as lines:
+    patterns = next(lines).rstrip().replace(" ", "").split(",")
+    count = counter(sorted(patterns, key=len, reverse=True))
+    next(lines)
+    counts = [count(line.rstrip()) for line in lines]
+
+
+print(sum(count > 0 for count in counts), sum(counts))
