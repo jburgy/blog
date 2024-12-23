f3bf632a46c81748c5a06ff53bd8825700879059
Jan Burgy <jburgy@gmail.com>
2024-12-23 11:47:52 -0500

    Simplify Day 23 of Advent of Code

--- aoc2024/day23.py
+++ aoc2024/day23.py
@@ -1,26 +1,14 @@
-from collections import defaultdict
-from itertools import product
-
 import networkx as nx
-
-edges = defaultdict(set)
-with open("aoc2024/day23input.txt", "rt") as lines:
-    for line in lines:
-        a, b = line.rstrip().split("-")
-        edges[a].add(b)
-        edges[b].add(a)
-
-triples = set()
-for node, nodes in edges.items():
-    if str(node).startswith("t"):
-        for a, b in product(nodes, nodes):
-            if b in edges[a]:
-                triples.add(tuple(sorted([a, b, node])))
-print(len(triples))
 
 g = nx.Graph()
 with open("aoc2024/day23input.txt", "rt") as lines:
     g.add_edges_from(line.rstrip().split("-") for line in lines)
+
+cycles = sum(
+    any(str(node).startswith("t") for node in cycle)
+    for cycle in nx.simple_cycles(g, length_bound=3)
+)
+print(cycles)
 
 clique = nx.approximation.max_clique(g)
 print(*sorted(clique), sep=",")
