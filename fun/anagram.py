80e68c37e969d3aaa27b964483ebbabe9aecdffa
Jan Burgy <jburgy@gmail.com>
2025-01-05 20:19:43 -0500

    Fix build

--- fun/anagram.py
+++ fun/anagram.py
@@ -3,12 +3,17 @@
 from sys import argv
 from itertools import permutations
 
-with open("fun/corncob_lowercase.txt", "rt") as lines:
-    words = set(map(str.rstrip, lines))
+if len(argv) > 1:
+    with open("fun/corncob_lowercase.txt", "rt") as lines:
+        words = set(map(str.rstrip, lines))
 
-for r in reversed(range(3, 7)):
-    print(
-        *sorted(
-            {word for t in permutations(argv[1], r=r) if (word := "".join(t)) in words}
+    for r in reversed(range(3, 7)):
+        print(
+            *sorted(
+                {
+                    word
+                    for t in permutations(argv[1], r=r)
+                    if (word := "".join(t)) in words
+                }
+            )
         )
-    )
