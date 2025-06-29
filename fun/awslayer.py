121b8fdee0f8f194e8c1b83f6dbe38e0f0f75dab
Jan Burgy <jburgy@gmail.com>
2025-06-29 09:39:01 -0400

    Tool to package AWS Lambda layer

---
+++ fun/awslayer.py
@@ -0,0 +1,64 @@
+"""Package your layer content for AWS Lambda.
+
+To create a layer, bundle your packages into a .zip file archive that meets the
+following requirements:
+
+* Build the layer using the same Python version that you plan to use for the Lambda
+function. For example, if you build your layer using Python 3.13, use the Python 3.13
+runtime for your function.
+* Your .zip file must include a python directory at the root level.
+* The packages in your layer must be compatible with Linux. Lambda functions run on
+Amazon Linux.
+* If your layer includes native binaries or executable files, they must target the same
+architecture (x86_64 or arm64) as your function.
+
+You can create layers that contain either third-party Python libraries installed with
+pip (such as requests or pandas) or your own Python modules and packages.
+
+See https://docs.aws.amazon.com/lambda/latest/dg/python-layers.html#python-layers-package
+"""
+
+import sysconfig
+from argparse import ArgumentParser
+from collections import deque
+from pathlib import Path
+from typing import Callable
+from zipfile import ZipFile
+
+platlib = Path(sysconfig.get_path("platlib"))
+
+def tail(max_lines: int) -> Callable[[str], None]:
+    lines = deque(maxlen=max_lines)
+    escape = "\N{ESC}"
+    clrscr = f"{escape}[{max_lines}F{escape}[J"
+
+    def inner(line: str) -> None:
+        full = len(lines) >= max_lines
+        lines.append(line)
+        if full:
+            print(clrscr, end="")
+            print(*lines, sep="\n", flush=True)
+        else:
+            print(line)
+
+    return inner
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("zipfile", nargs="?", default="layer.zip")
+    args = parser.parse_args()
+
+    taylor = tail(max_lines=5)
+    with ZipFile(args.zipfile, "w") as zf:
+        for root, dirs, files in platlib.walk():
+            taylor(str(root))
+            for file in files:
+                path = root / file
+                zf.write(
+                    filename=path, arcname=Path("python") / path.relative_to(platlib)
+                )
+
+
+if __name__ == "__main__":
+    main()
