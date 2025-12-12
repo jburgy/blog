22b7ee742828ee711328e7e88c6a8c15b8f8989b
Jan Burgy <jburgy@gmail.com>
2025-12-12 13:10:14 -0500

    This approach requires Deep Thought

---
+++ aoc2025/day12troll.py
@@ -0,0 +1,52 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "polyomino",
+# ]
+# ///
+from itertools import chain, repeat
+from pathlib import Path
+
+from polyomino import TilingProblem
+from polyomino.constant import MONOMINO
+from polyomino.board import Rectangle
+from polyomino.solution import Solution
+from polyomino.tileset import Tileset
+
+
+def cover(line: str, *, tiles: list[list[tuple[int, int]]]) -> Solution | None:
+    board, _, presents = line.partition(": ")
+    height, _, width = board.partition("x")
+
+    return TilingProblem(
+        board=Rectangle(int(height), int(width)),
+        tileset=Tileset(
+            mandatory=list(
+                chain.from_iterable(
+                    repeat(tile, int(count))
+                    for tile, count in zip(tiles, presents.split())
+                )
+            ),
+            optional=[],
+            filler=[MONOMINO],
+            reflections=True,
+        ),
+    ).solve()
+
+
+lines = Path("aoc2025/day12input.txt").read_text().splitlines()
+
+tiles = [
+    [
+        (i, j)
+        for i, line in enumerate(lines[start + 1 : start + 4])
+        for j, c in enumerate(line.rstrip())
+        if c == "#"
+    ]
+    for start in range(0, 30, 5)
+]
+
+for line in lines[30:]:
+    print(line)
+    solution = cover(line, tiles=tiles)
+    print(solution.display() if solution else "No solution")
