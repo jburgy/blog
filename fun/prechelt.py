78d70107aadd25526f682e6ee0946066577ef29b
Jan Burgy <jburgy@gmail.com>
2026-03-07 08:00:58 -0500

    Oy veh, ty

--- fun/prechelt.py
+++ fun/prechelt.py
@@ -48,11 +48,11 @@ def aho_corasick(*words):
         for digit in map(int, word.translate(MAP)):
             next = node[digit]
             if next is None:
-                node[digit] = next = [None] * WORD  # pyright: ignore[reportArgumentType, reportCallIssue]
+                node[digit] = next = [None] * WORD  # pyright: ignore[reportArgumentType, reportCallIssue]  # ty: ignore[invalid-assignment]
             node = next
         node.append(word)
 
-    root[FAIL] = root  # pyright: ignore[reportArgumentType, reportCallIssue]
+    root[FAIL] = root  # pyright: ignore[reportArgumentType, reportCallIssue]  # ty: ignore[invalid-assignment]
     stack = []
     append = stack.append
     for node in root[:FAIL]:
