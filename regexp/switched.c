1a8491403b901d88e8040f4811bf5aebedcca4e6
Jan Burgy <jburgy@gmail.com>
2026-09-25 16:02:18 -0400

    regexp: add switched.c, a switch-dispatched threaded.c
    Replaces the labels-as-values NEXT with a for(;;) switch over integer
    opcodes, so the opcodes no longer have to be addresses of labels inside
    search().  convert() and compile() therefore move out of search(), which
    now takes the compiled program: search(union cell *code, char *s).
    
    Output is byte-identical to threaded.c on all 14 tests.

---
+++ regexp/switched.c
@@ -0,0 +1,351 @@
+/*
+ * Switch-dispatched variant of threaded.c: the same on-the-fly
+ * compiler for Thompson's algorithm, but the interpreter is a plain
+ * `switch' over small integer opcodes rather than GCC's labels as
+ * values.  Because the opcodes no longer have to be the addresses of
+ * labels inside search(), convert() and compile() move out of it and
+ * search() takes the compiled program directly.
+ *
+ * See also Thompson, Ken.  Regular Expression Search Algorithm,
+ * Communications of the ACM 11(6) (June 1968), pp. 419-422.
+ *
+ * Copyright (c) 2004 Jan Burgy.
+ * Can be distributed under the MIT license, see bottom of file.
+ */
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum {
+    LPAREN = CHAR_MAX + 1,
+    RPAREN, /* This should  */
+    ALTERN, /* reflect the  */
+    CONCAT, /* precedence   */
+    KLEENE  /* rules!       */
+};
+
+static unsigned char *prepare(const char *src)
+{
+    unsigned char escape[CHAR_MAX + 1] = "";
+    unsigned char *dest = malloc(2 * (strlen(src) + 1));
+    int c, i, j = 0, concat = 0, nparen = 0;
+
+    escape['a'] = '\a';
+    escape['b'] = '\b';
+    escape['f'] = '\f';
+    escape['n'] = '\n';
+    escape['r'] = '\r';
+    escape['t'] = '\t';
+    escape['v'] = '\v';
+    for (i = 0; (c = "\"()*\\|"[i]); i++)
+        escape[c] = c;
+
+    for (i = 0; (c = src[i]); i++) {
+
+        switch (c) {
+
+            case '(':
+                if (concat)
+                    dest[j++] = CONCAT;
+                dest[j++] = LPAREN;
+                concat = 0;
+                nparen++;
+                continue;
+            case ')':
+                dest[j++] = RPAREN;
+                nparen--;
+                break;
+            case '*':
+                dest[j++] = KLEENE;
+                break;
+            case '|':
+                dest[j++] = ALTERN;
+                concat = 0;
+                continue;
+            case '\\':
+                c = escape[(int)src[i + 1]];
+                c ? i++ : (c = '\\');
+                __attribute__((fallthrough));
+            default:
+                if (concat)
+                    dest[j++] = CONCAT;
+                dest[j++] = c;
+        }
+        concat = 1;
+        if (nparen < 0)
+            printf("unbalanced parentheses\n");
+    }
+    dest[j++] = RPAREN;
+    dest[j++] = '\0';
+
+    return dest;
+}
+
+static unsigned char *convert(const char *src)
+{ /* http://cs.lasierra.edu/~ehwang/cptg454/postfix.pdf */
+    unsigned char stack[BUFSIZ] = "";
+    unsigned char *dest = prepare(src);
+    int c, i, j = 0, top = 0;
+
+    stack[top++] = LPAREN;
+    for (i = 0; (c = dest[i]); i++) {
+
+        switch (c) {
+
+            case LPAREN:
+                stack[top++] = c;
+                break;
+
+            case RPAREN:
+            case ALTERN:
+            case CONCAT:
+            case KLEENE:
+                while (c <= stack[top - 1])
+                    dest[j++] = stack[--top];
+                if (c == RPAREN)
+                    --top; /* discard LPAREN */
+                else
+                    stack[top++] = c;
+                break;
+
+            default:
+                dest[j++] = c;
+                break;
+        }
+    }
+    dest[j++] = '\0';
+
+    return dest;
+}
+
+/*
+ * A compiled program is an array of cells.  A cell is either an
+ * opcode -- one of the small integers below -- or that opcode's
+ * operand: a literal character, or a link to another cell.  The x86
+ * version encodes the same thing as a two byte `jmp' whose
+ * displacement has to be added to the address of the displacement
+ * byte itself; here a link is simply a pointer.
+ */
+union cell {
+    int op;
+    union cell *link;
+    int chr;
+};
+
+enum { JUMP,
+       CHAR,
+       FORK,
+       STOP,
+       FAIL,
+       XCHG }; /* XCHG never appears in compiled code, only in the sentinel */
+
+/*
+ * Node layouts, mirroring the 11/17/9 byte x86 encodings.  The leading
+ * JUMP of a node doubles as the successor link of the node before it,
+ * which is what makes concatenation free.
+ *
+ *                          char JUMP <body>  CHAR <c>  entry = body, exit = the cell after
+ *                          * FORK <body>  JUMP <exit>  FORK <body>  JUMP <exit>
+ *                            entry = the second FORK, whose JUMP recognizes lambda
+ *                          | JUMP <exit>  FORK <b>  JUMP <a> entry = the FORK, exit = the cell after
+ *
+ * lambda[] is Thompson's revision from the Notes of his paper: it points
+ * at the JUMP taken when a fragment matches the empty string, or is NULL.
+ * Starring a fragment turns that JUMP into FAIL so that a** cannot loop.
+ */
+static int codelen(const unsigned char *src)
+{
+    int i, c, n = 1; /* one cell for the trailing STOP */
+
+    for (i = 0; (c = src[i]); i++) {
+        switch (c) {
+            default:
+                n += 4;
+                break;
+            case CONCAT:
+                break;
+            case KLEENE:
+                n += 8;
+                break;
+            case ALTERN:
+                n += 6;
+                break;
+        }
+    }
+    return n;
+}
+
+static union cell *compile(const unsigned char *src)
+{
+    int i, c, top = 0;
+    union cell *stack[BUFSIZ], *lambda[BUFSIZ], *code, *pc;
+
+    code = pc = malloc(codelen(src) * sizeof *code);
+
+    /* clang-format off */
+    for (i = 0; (c = src[i]); i++) {
+
+        switch (c) {
+
+            default:
+                lambda[top] = NULL;
+                stack[top++] = pc + 1;
+                pc[0].op   = JUMP; pc[1].link = pc + 2;
+                pc[2].op   = CHAR; pc[3].chr  = c;
+                pc += 4;
+                break;
+
+            case CONCAT:
+                if (!lambda[top - 1])
+                    lambda[top - 2] = NULL;
+                --top;
+                break;
+
+            case KLEENE:
+                pc[0].op   = FORK; pc[1].link = stack[top - 1]->link;
+                pc[2].op   = JUMP; pc[3].link = pc + 8;
+                pc[4].op   = FORK; pc[5].link = stack[top - 1]->link;
+                pc[6].op   = JUMP; pc[7].link = pc + 8;
+                stack[top - 1]->link = pc + 4;
+                if (lambda[top - 1])
+                    lambda[top - 1]->op = FAIL;
+                lambda[top - 1] = pc + 6;
+                pc += 8;
+                break;
+
+            case ALTERN:
+                pc[0].op   = JUMP; pc[1].link = pc + 6;
+                pc[2].op   = FORK; pc[3].link = stack[top - 1]->link;
+                pc[4].op   = JUMP; pc[5].link = stack[top - 2]->link;
+                stack[top - 1]->link = pc + 6;
+                stack[top - 2]->link = pc + 2;
+                if (!lambda[top - 2])
+                    lambda[top - 2] = lambda[top - 1];
+                else if (lambda[top - 1])
+                    lambda[top - 1][1].link = lambda[top - 2];
+                pc += 6;
+                --top;
+                break;
+
+        }
+
+        /* clang-format on */
+    }
+    pc->op = STOP;
+
+    return code;
+}
+
+char *search(union cell *code, char *s)
+{
+    union cell xchg = {XCHG};
+    union cell *pc = &xchg;
+    union cell *clist[BUFSIZ], *nlist[BUFSIZ];
+    char *found = NULL;
+    int cnode = 0, nnode = 0, c = EOF; /* any non-NUL c primes the first exchange */
+
+    for (;;) {
+        switch ((pc++)->op) { /* the fetch-and-advance NEXT of threaded.c */
+
+            case XCHG:
+                /* CLIST is exhausted: swap the lists and plant XCHG as its sentinel */
+                if (!c)
+                    return found;
+                clist[cnode++] = &xchg;
+                while (nnode)
+                    clist[cnode++] = nlist[--nnode];
+                c = (unsigned char)*s++;
+                pc = code; /* unanchored, so start a fresh thread at every position */
+                break;
+
+            case JUMP:
+                pc = pc->link;
+                break;
+
+            case CHAR:
+                if ((pc++)->chr == c)
+                    nlist[nnode++] = pc; /* pc is the successor link, i.e. the continuation */
+                __attribute__((fallthrough));
+
+            case FAIL:
+                /* this thread is done for this character: run the next one on CLIST */
+                pc = clist[--cnode];
+                break;
+
+            case FORK:
+                clist[cnode++] = pc + 1; /* run the fall-through later, the branch now */
+                pc = pc->link;
+                break;
+
+            case STOP:
+                return s - 1;
+        }
+    }
+}
+
+int main(void)
+{
+    short i;
+    struct {
+        char *r;
+        char *s;
+    } test[] = {
+        {"abcdefg", "abcdefg"},
+        {"(a|b)*a", "ababababab"},
+        {"(a|b)*a", "aaaaaaaaba"},
+        {"(a|b)*a", "aaaaaabac"},
+        {"a(b|c)*d", "abccbcccd"},
+        {"a(b|c)*d", "abccbcccde"},
+        {"(a|a)*", "aaaaaaaaaaaaaaaaaaaaaaaaaaaa"},
+        {"a(b|c)*d", "abccccccccd"},
+        {"a*", "aaab"},
+        {"a(b|c)*d", "abcd"},
+        {"a**", "b"},
+        {"(a*b*)*c", "abbac"},
+        {"(a*|b*)*c", "abac"},
+        {"b*(c|d)", "c"},
+        {NULL, NULL}};
+
+    for (i = 0; test[i].r; i++) {
+        unsigned char *p = convert(test[i].r);
+        union cell *code = compile(p);
+        char *t;
+
+        free(p);
+        printf("search %s %s\n", test[i].r, test[i].s);
+        t = search(code, test[i].s);
+        free(code);
+        if (t)
+            printf("match found after %td bytes\n", t - test[i].s);
+        else
+            printf("match not found\n");
+    }
+
+    return 0;
+}
+
+/*
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall
+ * be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
